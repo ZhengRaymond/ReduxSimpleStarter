01505d648f9273c627e41ee578f4bce4731126cf
ZhengRaymond <raymond1wen1zheng1@gmail.com>
2017-07-25 12:51:47 -0400

    bookList mini-app

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Book 1', pages: 101 },
+    { title: 'Book 2', pages: 39 },
+    { title: 'Book 3', pages: 85 },
+    { title: 'Book 4', pages: 1 }
+  ]
+}
